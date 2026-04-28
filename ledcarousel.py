9bb8c3e879f0b586e8a3cefb408ce471a0faa9b8
Anya S <anya.skliarets@gmail.com>
2026-04-28 16:58:30 +0100

    Added Python version of the program

---
+++ ledcarousel.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import time
+
+def green():
+	with open("/sys/class/leds/hub:green:led1/brightness", "a") as f:
+  		f.write("225")
+  	time.sleep(1)
+  	with open("/sys/class/leds/hub:green:led1/brightness", "a") as f:
+  		f.write("0")
+
+def blue():
+	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
+  		f.write("225")
+  	time.sleep(1)
+  	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
+  		f.write("0")
+
+def red():
+	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("225")
+  	time.sleep(1)
+  	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("0")
+
+def white():
+	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("225")
+  	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
+  		f.write("225")
+  	with open("/sys/class/leds/hub:green:led1/brightness", "a") as f:
+  		f.write("225")
+  	time.sleep(1)
+  	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("0")
+  	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
+  		f.write("0")
+  	with open("/sys/class/leds/hub:green:led1/brightness", "a") as f:
+  		f.write("0")
+
+def purple():
+	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("225")
+  	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
+  		f.write("225")
+  	time.sleep(1)
+  	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("0")
+  	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
+  		f.write("0")
+
+# main program
+red()
+time.sleep(0.5)
+red()
+time.sleep(0.5)
+red()
+time.sleep(0.5)
+blue()
+time.sleep(0.5)
+blue()
+time.sleep(0.5)
+blue()
+time.sleep(0.5)
+green()
+sleep.time(0.5)
+green()
+sleep.time(0.5)
+green()
+sleep.time(0.5)
+yellow()
+sleep.time(0.5)
+yellow()
+sleep.time(0.5)
+yellow()
+sleep.time(0.5)
