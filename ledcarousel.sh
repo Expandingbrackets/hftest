803835888c64bc6849c4ad2f4c49d8a6e0467e2f
Anya S <anya.skliarets@gmail.com>
2026-04-28 14:15:47 +0100

    Added comments

--- ledcarousel.sh
+++ ledcarousel.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+#the LED turns green for 2 seconds and then turns off
 green() {
 	echo "225" > /sys/class/leds/hub:green:led1/brightness
 	sleep 2
 	echo "0" > /sys/class/leds/hub:green:led1/brightness
 }
-
+#the LED turns yellow for 2 seconds and then turns off
 yellow() {
 	echo "225" > /sys/class/leds/hub:red:led3/brightness
 	echo "225" > /sys/class/leds/hub:green:led1/brightness
@@ -12,19 +13,19 @@ yellow() {
 	echo "0" > /sys/class/leds/hub:red:led3/brightness
 	echo "0" > /sys/class/leds/hub:green:led1/brightness
 }
-
+#the LED turns red for 2 seconds and then turns off
 red() {
 	echo "225" > /sys/class/leds/hub:red:led3/brightness
 	sleep 2
 	echo "0" > /sys/class/leds/hub:red:led3/brightness
 }
-
+#the LED turns blue for 2 seconds and then turns off
 blue() {
 	echo "225" > /sys/class/leds/hub:blue:led2/brightness
 	sleep 2
 	echo "0" > /sys/class/leds/hub:blue:led2/brightness
 }
-
+#the LED turns purple for 2 seconds and then turns off
 purple() {
 	echo "225" > /sys/class/leds/hub:red:led3/brightness
 	echo "225" > /sys/class/leds/hub:blue:led2/brightness
@@ -32,8 +33,8 @@ purple() {
 	echo "0" > /sys/class/leds/hub:red:led3/brightness
 	echo "0" > /sys/class/leds/hub:blue:led2/brightness
 }
-
-pink() {
+#the LED turns white for 2 seconds and then turns off
+white() {
 	echo "225" > /sys/class/leds/hub:red:led3/brightness
 	echo "192" > /sys/class/leds/hub:green:led1/brightness
 	echo "203" > /sys/class/leds/hub:blue:led2/brightness
@@ -47,7 +48,7 @@ pink() {
 for i in seq 1 2; do
 	green
 	yellow
-	pink
+	white
 	red
 	purple
 	blue
