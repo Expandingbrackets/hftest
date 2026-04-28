6c77095769d9332583f4e4ecbc320eafa0a6da0a
Anya S <anya.skliarets@gmail.com>
2026-04-28 13:54:37 +0100

    Added the actual script to the project; now the HUB's LED can change colours in this order: green, yellow, white, red, purple, blue

--- ledcarousel.sh
+++ ledcarousel.sh
@@ -1,3 +1,54 @@
-#!/bi/sh
+#!/bin/sh
+green() {
+	echo "225" > /sys/class/leds/hub:green:led1/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:green:led1/brightness
+}
 
-echo 'Hello Hive HUB!'
+yellow() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	echo "225" > /sys/class/leds/hub:green:led1/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+	echo "0" > /sys/class/leds/hub:green:led1/brightness
+}
+
+red() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+}
+
+blue() {
+	echo "225" > /sys/class/leds/hub:blue:led2/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:blue:led2/brightness
+}
+
+purple() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	echo "225" > /sys/class/leds/hub:blue:led2/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+	echo "0" > /sys/class/leds/hub:blue:led2/brightness
+}
+
+pink() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	echo "192" > /sys/class/leds/hub:green:led1/brightness
+	echo "203" > /sys/class/leds/hub:blue:led2/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+	echo "0" > /sys/class/leds/hub:green:led1/brightness
+	echo "0" > /sys/class/leds/hub:blue:led2/brightness
+}
+
+#main program
+for i in seq 1 2; do
+	green
+	yellow
+	pink
+	red
+	purple
+	blue
+done
