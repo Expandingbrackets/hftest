c14ac05e18b74cbf3f0c9e4f95611b6e878bf0b3
askymango <anya.skliarets@gmail.com>
2026-04-28 14:49:28 +0100

    Finished main task - carousel of colours
    * Added new file to the project
    
    * Added the actual script to the project; now the HUB's LED can change colours in this order: green, yellow, white, red, purple, blue
    
    * Added comments
    
    * Added comments v2
    
    * Changed the code according to the plan(sadly)

---
+++ ledcarousel.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# LED turns green for 2 seconds and then turns off
+green() {
+	echo "225" > /sys/class/leds/hub:green:led1/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:green:led1/brightness
+}
+
+# LED turns yellow for 2 seconds and then turns off
+yellow() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	echo "225" > /sys/class/leds/hub:green:led1/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+	echo "0" > /sys/class/leds/hub:green:led1/brightness
+}
+
+# LED turns red for 2 seconds and then turns off
+red() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+}
+
+# LED turns blue for 2 seconds and then turns off
+blue() {
+	echo "225" > /sys/class/leds/hub:blue:led2/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:blue:led2/brightness
+}
+
+# LED turns purple for 2 seconds and then turns off
+purple() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	echo "225" > /sys/class/leds/hub:blue:led2/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+	echo "0" > /sys/class/leds/hub:blue:led2/brightness
+}
+
+# LED turns white for 2 seconds and then turns off
+white() {
+	echo "225" > /sys/class/leds/hub:red:led3/brightness
+	echo "192" > /sys/class/leds/hub:green:led1/brightness
+	echo "203" > /sys/class/leds/hub:blue:led2/brightness
+	sleep 2
+	echo "0" > /sys/class/leds/hub:red:led3/brightness
+	echo "0" > /sys/class/leds/hub:green:led1/brightness
+	echo "0" > /sys/class/leds/hub:blue:led2/brightness
+}
+
+#main program
+for i in seq 1 2; do
+	red
+	sleep 0.5
+	red
+	sleep 0.5
+	red
+	sleep 0.5
+	blue
+	sleep 0.5
+	blue
+	sleep 0.5
+	blue
+	sleep 0.5
+	green
+	sleep 0.5
+	green
+	sleep 0.5
+	green
+	sleep 0.5
+	yellow
+	sleep 0.5
+	yellow
+	sleep 0.5
+	yellow
+done
