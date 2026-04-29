292e53c587a67ab5945b904c9b2f583556023573
Anya S <anya.skliarets@gmail.com>
2026-04-29 10:38:35 +0100

    Added colour yellow and fixed the error with time

--- ledcarousel.py
+++ ledcarousel.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import time
 
 def green():
@@ -48,6 +48,17 @@ def purple():
   	with open("/sys/class/leds/hub:blue:led2/brightness", "a") as f:
   		f.write("0")
 
+ def yellow():
+ 	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("225")
+  	with open("/sys/class/leds/hub:green:led1/brightness", "a") as f:
+  		f.write("225")
+  	time.sleep(1)
+  	with open("/sys/class/leds/hub:red:led3/brightness", "a") as f:
+  		f.write("0")
+  	with open("/sys/class/leds/hub:green:led1/brightness", "a") as f:
+  		f.write("0")
+
 # main program
 red()
 time.sleep(0.5)
@@ -62,14 +73,14 @@ time.sleep(0.5)
 blue()
 time.sleep(0.5)
 green()
-sleep.time(0.5)
+time.sleep(0.5)
 green()
-sleep.time(0.5)
+time.sleep(0.5)
 green()
-sleep.time(0.5)
+time.sleep(0.5)
 yellow()
-sleep.time(0.5)
+time.sleep(0.5)
 yellow()
-sleep.time(0.5)
+time.sleep(0.5)
 yellow()
-sleep.time(0.5)
+time.sleep(0.5)
